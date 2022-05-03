f050f3e6516f1431dee49ebd48c97597dc14cf1e
Michael Burke <michael.dburke56@gmail.com>
2022-05-02 20:22:43 -0400

    Implement most basic form of ERC20 contract.

--- contracts/Sk8Token.sol
+++ contracts/Sk8Token.sol
@@ -1,6 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-contract Sk8Token {
-  
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Sk8Token is ERC20 {
+  constructor() ERC20("Sk8Token", "SK8") {}
+
+  function mint(address to, uint256 amount) public {
+    _mint(to, amount);
+  }
+
 }
