c53747e8fe89f933360807afd29b4a4d8cf2d72f
Michael Burke <michael.dburke56@gmail.com>
2022-05-02 21:08:03 -0400

    Implement two access roles, add modifyers to functions and asign DEFAULT_ADMIN_ROLE to deployer of contract.

--- contracts/Sk8Token.sol
+++ contracts/Sk8Token.sol
@@ -1,23 +1,34 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
+import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 
-contract Sk8Token is ERC20, ERC20Burnable, Pausable {
-  constructor() ERC20("Sk8Token", "SK8") {}
+contract Sk8Token is ERC20, AccessControl, ERC20Burnable, Pausable {
+  bytes32 public constant MINT_AND_BURN_ROLE = keccak256("MINT_AND_BURN_ROLE");
+  bytes32 public constant MAINTAIN_ROLE = keccak256("MAINTAIN_ROLE");
 
-  function pause() public {
+  constructor() ERC20("Sk8Token", "SK8") {
+    // @dev Grant the deployer of the contract the highest admin level. This supercede all other roles, but also be able to asign other roles. 
+    _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+  }
+
+  function pause() public onlyRole(MAINTAIN_ROLE) {
     _pause();
   }
 
-  function unpause() public {
+  function unpause() public onlyRole(MAINTAIN_ROLE) {
     _unpause();
   }
 
-  function mint(address to, uint256 amount) public {
+  function mint(address to, uint256 amount) public onlyRole(MINT_AND_BURN_ROLE) {
     _mint(to, amount);
+  }
+
+  function burn(address from, uint256 amount) public onlyRole(MINT_AND_BURN_ROLE) {
+    _burn (from, amount);
   }
 
   function _beforeTokenTransfer(address from, address to, uint256 amount)
