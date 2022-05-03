60c98eb5285039644dcb3d2e118d4b56bf17c02c
Michael Burke <michael.dburke56@gmail.com>
2022-05-03 17:40:16 -0400

    Set up truffle-config for rinkeby deployment.

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,7 @@
+require('dotenv').config();
+
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
 /**
  * Use this file to configure your truffle project. It's seeded with some
  * common settings for different networks and features like migrations,
@@ -22,7 +26,11 @@
 //
 // const fs = require('fs');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
+const acctOneKeys = process.env.RINKEBY_ACCT1_PRIVATE_KEYS;
 
+const infuraEndPoint = process.env.INFURA_ENDPOINT;
+
+const private_keys = [acctOneKeys];
 module.exports = {
   /**
    * Networks define how you connect to your ethereum client and let you set the
@@ -57,14 +65,18 @@ module.exports = {
     // },
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-    // network_id: 3,       // Ropsten's id
-    // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-    // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    rinkeby: {
+      provider: () => new HDWalletProvider({
+        privateKeys: private_keys,
+        providerOrUrl: infuraEndPoint,
+        numberOfAddresses: 1
+      }),
+      network_id: 4,       // Rinkeby id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
     // Useful for private networks
     // private: {
     // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
